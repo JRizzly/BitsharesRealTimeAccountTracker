b3a385ef430ca93632cc0ce302b5a91198826259
JRizzly <JRizzly@gmail.com>
2019-05-24 10:15:24 -0500

    Validated Output
    I have validated output is consistent to my initial-less-automated method of figuring performance with excel.... The way it spits out data now fits into my excel file, and can quickly show similar findings.

--- justDataPull.py
+++ justDataPull.py
@@ -5,11 +5,13 @@ import time
 from bitshares.account import Account
 from bitshares.market import Market
 
+
 accountName = "jr-12"  #Change this to your account
 
 filename = "dataPull.csv"  #str(datetime.date())
 f = open(filename, 'a')
-		
+
+
 account = Account(accountName)
 f.write(str(account.balances))
 
--- mainBTS.py
+++ mainBTS.py
@@ -1,8 +1,23 @@
 import requests
 import json
+import bitshares
+import datetime
+import time
+from bitshares.account import Account
+from bitshares.market import Market
+from bitshares.asset import Asset
+
+
+
+#a = ( Asset('1.3.0') )#['symbol'] )
+
+
+
 
 #modify Request: https://wrapper.elasticsearch.bitshares.ws/apidocs/#!/wrapper/get_account_history
 #Translate Request to Python: https://curl.trillworks.com/#
+
+
 
 
 headers = {
@@ -12,7 +27,7 @@ headers = {
 params = (
     ('account_id', '1.2.1612456'),
     ('operation_type', '4'),
-    ('size', '100'),
+    ('size', '9999'),
     ('to_date', 'now'),
     ('sort_by', '-block_data.block_time'),
     ('type', 'data'),
@@ -29,29 +44,46 @@ IdTable = { #Will need to have this look up via python-bitshares later , Using T
     '1.3.121' : "USD",
 }
 
-
-
-
+json_data.reverse()
 
 for i in range(0, len(json_data)):
     #print (json_data[i]['operation_history']['op_object']['is_maker'])
 
-    payAsset = IdTable[  json_data[i]['operation_history']['op_object']['pays']['asset_id']  ]  #Temp for understanding
-    payAssetAmount = json_data[i]['operation_history']['op_object']['pays']['amount']/10000
+    payAsset = Asset(  json_data[i]['operation_history']['op_object']['pays']['asset_id'] )['symbol']  #Temp for understanding
+    payAssetAmount = json_data[i]['operation_history']['op_object']['pays']['amount'] #/10000
+    if (payAsset == "USD" ):
+        payAssetAmount = payAssetAmount /10000
+    if (payAsset == "BTS" ):
+        payAssetAmount = payAssetAmount /100000
 
     receiveAsset = IdTable[ json_data[i]['operation_history']['op_object']['receives']['asset_id'] ] #temp
-    receiveAssetAmount = json_data[i]['operation_history']['op_object']['receives']['amount']/100000
+    receiveAssetAmount = json_data[i]['operation_history']['op_object']['receives']['amount'] #/100000
+    if (receiveAsset == "USD" ):
+        receiveAssetAmount = receiveAssetAmount /10000
+    if (receiveAsset == "BTS" ):
+        receiveAssetAmount = receiveAssetAmount /100000
 
+
+
+
+
+    ''' 
     print( "Paid Assest: " + str(payAsset) + " At this Amount: " + str(payAssetAmount) )
     print("Received Assest: " + str(receiveAsset) + " At this Amount: " + str(receiveAssetAmount) )
     print( "Receive Asset Price: " + str( receiveAsset ) + " " +  str( receiveAssetAmount / payAssetAmount  )  )
     print("Paid Asset Price: " + str(payAsset) + " " + str(payAssetAmount / receiveAssetAmount))
+    '''
 
+    print(str(receiveAssetAmount) + "," + str(receiveAsset) , end=",")
+    print( str(payAssetAmount) + "," + str(payAsset) )
+
+    #print( "Receive Asset Price: " + str( receiveAsset ) + " " +  str( receiveAssetAmount / payAssetAmount  )  )
+    #print("Paid Asset Price: " + str(payAsset) + " " + str(payAssetAmount / receiveAssetAmount))
 
 
 
     #print (json_data[i]['operation_history']['op_object']['pays']['asset_id'] + " " + str(json_data[i]['operation_history']['op_object']['pays']['amount'])   )
-    print()
+    #print()
 
 
 
